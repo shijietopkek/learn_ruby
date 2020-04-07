7ec9a516f6159c26e31d477fa0d46ce1c0681ed2
shijietopkek <yu.shijie@dhs.sg>
2020-04-07 23:58:09 +0800

    initial commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add(x,y)
+    x+y
+end
+
+def subtract(x,y)
+    x-y 
+end
+
+def sum(array)
+    sum=0
+    (array.length).times do |i|
+        sum+=array[i]
+    end
+    sum
+end
+
+def multiply(array)
+    product = 1
+    (array.length).times do |i|
+        product *= array[i]
+    end
+    product
+end
+
+def power(base,exp)
+    base**exp
+end
+
+def factorial(num)
+    if num==0
+        1
+    elsif num==1
+        1
+    else
+        num * factorial(num-1)
+    end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo(sentence)
+    sentence
+end
+
+def shout(sentence)
+    sentence.upcase
+end
+
+def repeat(sentence, n=2)
+    ([sentence]*n).join(' ')
+end
+
+def start_of_word(word, n=1)
+    word[0..n-1]
+end
+
+def first_word(sentence)
+    array = sentence.split(' ')
+    array[0]
+end
+
+def titleize(sentence)
+    array = sentence.split(' ')
+    (array.length).times do |i|
+        if (i!=0 &&(array[i]=='and'||array[i]=='over'|| array[i]=='the'))
+            next
+        else
+            array[i]=array[i].capitalize
+        end
+    end
+    sentence = array.join(' ')
+    sentence
+end
