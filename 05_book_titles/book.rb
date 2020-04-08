70596123e5acd44b6fe5ef522c2f89080d7f374c
shijietopkek <yu.shijie@dhs.sg>
2020-04-08 10:20:19 +0800

    last 2 projects

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
 # write your code here
+    attr_accessor :title
+
+    def title
+        special = ['a','an','the','and','of','in']
+
+        titles = @title.split(' ')
+        (titles.length).times do |i|
+            titles[i] = titles[i].downcase
+            if titles[i]=='i'
+                titles[i] = titles[i].upcase
+            elsif (special.include? titles[i])
+                next
+            else
+                titles[i] = titles[i].capitalize
+            end
+        end
+        titles[0] = titles[0].capitalize
+        titles.join(' ')
+    end
+
+        
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hour = 0
+    minute = 0
+    second = 0
+
+    time = @seconds
+    
+    while time >= 3600 
+      hour+=1 
+      time-=3600
+    end
+    while time >= 60
+      minute+=1
+      time-=60
+    end
+
+    second = time
+
+    hour.to_s.rjust(2,'0')+":"+minute.to_s.rjust(2,"0")+":"+second.to_s.rjust(2,"0")
+  end
+
 end
