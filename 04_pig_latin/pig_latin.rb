03bd24f6b5a839cf117e653e77162a39281f7ace
shijietopkek <yu.shijie@dhs.sg>
2020-04-08 08:45:49 +0800

    pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,45 @@
 #write your code here
+def to_pig_latin(word)
+    vowels='aeiou'
+    index=-1
+    capitalized = false
+    if word == word.capitalize
+        capitalized = true
+    end
+    word=word.downcase
+    (word.length).times do |i|
+        if ((vowels.include? word[i]) == false)
+            index+=1
+        elsif (word[i]=='u' && word[i-1]=='q')
+            index+=1
+        else
+            break
+        end
+    end
+    if index==-1
+        front = word 
+        back=""
+    else
+        front = word[0..index]
+        back = word[index+1..word.length]
+    end
+    if (capitalized)
+        back.capitalize + front.downcase + "ay"
+    else
+        back + front + 'ay'
+    end
+end
+
+def translate(sentence)
+    words = sentence.split(' ')
+    (words.length).times do |i|
+        words[i] = to_pig_latin(words[i])
+    end
+    new_sentence = words.join(' ')
+    new_sentence
+end
+
+
+
+
+
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -68,5 +68,7 @@ describe "#translate" do
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
-
+  it "capitalized words are still capitalized" do
+    expect(translate("The quick brown fox")).to eq("Ethay ickquay ownbray oxfay")
+  end
 end
